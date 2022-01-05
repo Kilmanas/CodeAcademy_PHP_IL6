11c2ebd961ec2a16d6c9222491814fffc0b1b582
Kilmanas <m.kilmanas@gmail.com>
2022-01-05 22:05:29 +0200

    funkcijos

--- Forms/functions.php
+++ Forms/functions.php
@@ -14,20 +14,68 @@
 //    echo 'Neteisingas';
 //}
 
-$x = $_POST['number1'];
-$y = $_POST['number2'];
+//$x = $_POST['number1'];
+//$y = $_POST['number2'];
+//
+//switch ($_POST['operation']){
+//    case '+':
+//        echo $x + $y;
+//        break;
+//    case '-':
+//        echo $x - $y;
+//        break;
+//    case '*':
+//        echo $x * $y;
+//        break;
+//    case '/':
+//        echo $x / $y;
+//        break;
+//}
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$password1 = $_POST['password'];
+$password2 = $_POST['RepPassword'];
 
-switch ($_POST['veiksmas']){
-    case '+':
-        echo $x + $y;
-        break;
-    case '-':
-        echo $x - $y;
-        break;
-    case '*':
-        echo $x * $y;
-        break;
-    case '/':
-        echo $x / $y;
-        break;
+function clearEmail($email)
+{
+    $emailLowercases = strtolower($email);
+    return trim($emailLowercases);
+}
+function isEmailValid($email){
+    if (strpos($email, '@')){
+    return true;
+    }
+    return false;
+}
+if(isEmailValid($email)){
+    function getNickName($name, $surname)
+    {
+        return strtolower(substr($name, 0, 3) . substr($surname, 0, 3));
+    }
+}else{
+    echo 'Registration invalid';
+    return;
+}
+function isPasswordValid($password1, $password2 )
+{
+    if ($password1 == $password2) {
+        return true;
+    }
+    return false;
+}
+if(isPasswordValid($password1, $password2)){
+    echo 'Name:'.$name;
+    echo '<br>';
+    echo 'Surname:'.$surname;
+    echo '<br>';
+    echo 'Email:'.clearEmail($email);
+    echo '<br>';
+    echo 'Nickname:'.getNickName($name, $surname);
+    echo '<br>';
+    echo 'Registration successful';
+
+}else {
+    echo 'Registration invalid';
+    return;
 }
--- Forms/index.php
+++ Forms/index.php
@@ -1,8 +1,8 @@
 <html>
-    <head>
-        <title>Musu title</title>
-    </head>
-    <body>
+<head>
+    <title>Musu title</title>
+</head>
+<body>
 <!--        <div class="header">-->
 <!--            <ul>-->
 <!--                <li><a href="#">Home</a></li>-->
@@ -10,21 +10,29 @@
 <!--                <li><a href="#">About</a></li>-->
 <!--            </ul>-->
 <!--        </div>-->
-        <div class="content">
-            <h1>Calculator</h1>
-<!--            <p>me speak no latino</p>-->
-            <form action="functions.php" method="post">
-                <input type="number" name="number1" placeholder="Pirmas skaičius">
-                <input type="number" name="number2" placeholder="Antras skaičius">
-                <select name="veiksmas">
-                    <option value="+">sudėtis</option>
-                    <option value="-">atimtis</option>
-                    <option value="*">daugyba</option>
-                    <option value="/">dalyba</option>
-                </select>
-                <input type="submit" value="OK" name="submit" />
+<div class="content">
+    <h1>Registration</h1>
+    <!--            <p>me speak no latino</p>-->
+    <form action="functions.php" method="post">
+<!--        <input type="number" name="number1" placeholder="Pirmas skaičius">-->
+<!--        <input type="number" name="number2" placeholder="Antras skaičius">-->
+<!--        <select name="operation">-->
+<!--            <option value="+">sudėtis</option>-->
+<!--            <option value="-">atimtis</option>-->
+<!--            <option value="*">daugyba</option>-->
+<!--            <option value="/">dalyba</option>-->
+<!--        </select>-->
+        <ul>
+            <li><input type="text" name="name" placeholder="Name"></li>
+            <li><input type="text" name="surname" placeholder="Surname"></li>
+            <li><input type="text" name="email" placeholder="Email"></li>
+            <li><input type="text" name="password" placeholder="Password"></li>
+            <li><input type="text" name="RepPassword" placeholder="Repeat Password"></li>
 
-            </form>
-        </div>
-    </body>
+        </ul>
+        <input type="submit" value="Submit" name="submit"/>
+
+    </form>
+</div>
+</body>
 </html>
