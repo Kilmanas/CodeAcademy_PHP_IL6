2ab5bbc0d6c5375935e0ee51052d3b726da66681
Kilmanas <m.kilmanas@gmail.com>
2022-01-19 20:27:19 +0200

    avtomagazin

--- gumtree/parts/header.php
+++ gumtree/parts/header.php
@@ -5,8 +5,10 @@
 <body>
 <div class="header">
     <ul class="menu">
+        <li><a href="index.php">Home</a></li>
         <li><a href="create.php">Add Ad!</a></li>
         <li><a href="catalog.php">All ads!</a></li>
+        <li><a href="register.php">Register</a></li>
     </ul>
 </div>
 <div class="content">
--- gumtree/user.php
+++ gumtree/user.php
@@ -16,7 +16,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Add submitted";
+    echo "User registration successful";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
